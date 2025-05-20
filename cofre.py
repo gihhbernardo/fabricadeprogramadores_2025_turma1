26de7e81ed0430651ede18e7bd39eb6b7bfb5f38
gihhbernardo <geovanna.bernardofrasao@gmail.com>
2025-05-20 11:47:17 -0300

    transferir dados

---
+++ cofre.py
@@ -0,0 +1,3 @@
+import senha
+nova_senha=input()
+print(senha.verificar_senha(nova_senha))
---
+++ senha.py
@@ -0,0 +1,7 @@
+minha_senha="123456"
+
+def verificar_senha(nova_senha):
+    if (nova_senha) == (minha_senha):
+        print("valida")
+    else:
+        print("invalida")
