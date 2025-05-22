2d5df5626f6085b4b1445914604991b651acf6c9
gihhbernardo <geovanna.bernardofrasao@gmail.com>
2025-05-22 11:25:19 -0300

    numero aleatorios

---
+++ numeroaleatorio.py
@@ -0,0 +1,19 @@
+import random
+zoologico=["girafa","pinguim","elefante","cobra", "golfinho", "leão","tigre","onça pintada", "tartaruga","ouriço"]
+print(random.choice(zoologico))
+
+import random
+cesta=["folhas", "peixe", "vegetais","ratos","salmao", "carne","coelho","capivaras","colve", "larvas"]
+print(random.choice(cesta))
+
+import turtle 
+turtle.circle(200)
+turtle.getscreen()._root.mainloop()
+
+import this
+
+x=10
+while x>=0:
+    print(x)
+    x=x-1
+print("fogo!")
