03b6a113ba5e4db708b4446dba294389bee7fc54
gihhbernardo <geovanna.bernardofrasao@gmail.com>
2025-06-04 17:26:56 -0300

    aula de dicionario

---
+++ Listagem 6.53 - Exemplo de dicionário com estoque e operações de venda.py
@@ -0,0 +1,28 @@
+estoque={   "tomate": [ 1000, 2.30],
+            "alface": [ 500, 0.45],
+			"batata": [ 2001, 1.20],
+			"feijão": [ 100, 1.50] }
+produto= input("digite o produto:")
+venda = [[produto,float(input("digite a quantidade:"))]] 
+total = 0
+if (produto in estoque):
+
+  print("Vendas:\n")
+
+  for operação in venda:
+      produto, quantidade = operação 
+      preço = estoque[produto][1] 
+      custo = preço * quantidade
+      print("%12s: %3d x %6.2f = %6.2f" %
+  		(produto, quantidade,preço,custo))
+      estoque[produto][0] -= quantidade 
+      total+=custo
+  print(" Custo total: %21.2f\n" % total)
+  print("Estoque:\n")
+  for chave, dados in estoque.items(): 
+      print("Descrição: ", chave)
+      print("Quantidade: ", dados[0])
+      print("Preço: %6.2f\n" % dados[1])
+      print("produto ja existe no estoque")
+    
+  
