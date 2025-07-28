7fc8a987796d312fa5437ef344d1a6be6120913a
Geovanna Bernardo <geovanna.bernardofrasao@gmail.com>
2025-07-28 17:37:22 -0300

    variaveis revisão

---
+++ variavel.py
@@ -0,0 +1,49 @@
+def maior():
+ a=3
+ b=2
+ c=4
+ d=10
+ print(c!=a)
+ print(a==d)
+ print(b>d)
+ print(a<c)
+ print(b<=c)
+ print(a!=d)
+ print(d==b)
+ print(c<=a)
+ print(c!=d)
+ print(d!=b)
+maior()
+
+def logicos():
+    a=True
+    b=False
+    c=True
+    print(a and a)
+    print(b and b)
+    print(not c)
+    print(not a)
+    print(a and b)
+    print(b and c)
+    print(a or c)
+    print(not b)
+    print(a or b)
+    print(b or c)
+logicos()
+
+def aridmetica():  
+   a=3
+   b=2
+   c=4
+   d=10
+   print(a+b)
+   print(b-a)
+   print(c*d)
+   print(a/d)
+   print(b+c)
+   print(b**a)
+   print(d-c)
+   print(c+b)
+   print(a*d)
+   print(b**d)
+aridmetica()
