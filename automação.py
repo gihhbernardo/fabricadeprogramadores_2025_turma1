163024da7f79a110ac924f6dfbcb59d26b0388da
Geovanna Bernardo <geovanna.bernardofrasao@gmail.com>
2025-07-31 16:53:00 -0300

    Add files via upload
    automação pronto

--- automação.py
+++ automação.py
@@ -1,12 +1,27 @@
 import mouseinfo, pyautogui, time
 
 
-mouseinfo.mouseInfo()
+#mouseinfo.mouseInfo()
 
-pyautogui.moveTo(987,1061,duration=2)
-pyautogui.moveTo(737,138,duration=2)
+pyautogui.moveTo(duration=2)
+pyautogui.moveTo(988,1055,duration=2)
 pyautogui.click()
 time.sleep(1)
-pyautogui.write("facebook")
-time.sleep(1)
+pyautogui.moveTo(822,134,duration=2)
 pyautogui.click()
+time.sleep(1)
+pyautogui.write("youtube")
+pyautogui.moveTo(495,130,duration=2)
+pyautogui.click()
+time.sleep(1)
+pyautogui.moveTo(242,488,duration=2)
+pyautogui.click()
+time.sleep(1)
+pyautogui.moveTo(819,144,duration=2)
+pyautogui.write("poesia acustica")
+pyautogui.moveTo(1200,98,duration=2)
+pyautogui.click()
+time.sleep(1)
+pyautogui.moveTo(1200,98,duration=2)
+
+
